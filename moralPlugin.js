a7f0cf245059ed55f165a08a76040571c5058eb0
Kevin Akkoyun <kevin.akkoyun@gmail.com>
2021-07-02 12:14:54 +0300

    moralPlugin initial commit

---
+++ moralPlugin.js
@@ -0,0 +1,27 @@
+/**
+ * @author Hudrud
+ * @plugindesc Moral plugin for simple karma system
+ * 
+ * @param InitMoralValue
+ * @desc Intial moral value
+ * @type number
+ * @default 0
+ *
+ * @param MaxMoralValue
+ * @desc Maximum moral value
+ * @type number
+ * @default 100
+ * 
+ * @param MinMoralValue
+ * @desc Minimum moral value
+ * @type number
+ * @default 0
+ * 
+ */
+
+(function(){
+    let params = PluginManager.parameters("moralPlugin");
+
+    console.log(params)
+    
+})();
