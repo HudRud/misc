e79247172ac0118781636e8ecee865cfa21b2d7e
Kevin Akkoyun <kevin.akkoyun@gmail.com>
2021-07-02 12:27:25 +0300

    sync

--- moralPlugin.js
+++ moralPlugin.js
@@ -1,27 +1,30 @@
-/**
+/*:
  * @author Hudrud
  * @plugindesc Moral plugin for simple karma system
  * 
- * @param InitMoralValue
+ * @param initMoralValue
  * @desc Intial moral value
  * @type number
  * @default 0
  *
- * @param MaxMoralValue
+ * @param maxMoralValue
  * @desc Maximum moral value
  * @type number
  * @default 100
  * 
- * @param MinMoralValue
+ * @param minMoralValue
  * @desc Minimum moral value
  * @type number
  * @default 0
  * 
  */
 
+let currentMoralValue, minMoralValue,maxMoralValue
 (function(){
     let params = PluginManager.parameters("moralPlugin");
-
-    console.log(params)
+    currentMoralValue = params["initMoralValue"]
+    minMoralValue = params["minMoralValue"]
+    maxMoralValue = params["maxMoralValue"]
+    console.log(currentMoralValue + "\n" + minMoralValue + "\n" + maxMoralValue)
     
 })();
